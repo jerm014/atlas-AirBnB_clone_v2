dbf1b388f1d14fb4b34e5bfa17c0f468ea8537bf
Jeremy Mitts <jermitts@gmail.com>
2024-06-27 11:42:01 -0500

    test sql added

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,4 +5,4 @@ CREATE USER IF NOT EXISTS 'localhost'@'hbnb_dev' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant all privileges to the new user on the database.
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'localhost'@'hbnb_dev';
 -- hbnb_dev should have SELECT privilege on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'localhost'@'hbnb;
+GRANT SELECT ON performance_schema.* TO 'localhost'@'hbnb_dev';
