06dc8bbcedf70963963e1b334835b271562423a1
Jeremy Mitts <jermitts@gmail.com>
2024-06-27 15:02:12 -0500

    update for task6

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,32 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+# add in the class BaseModel:
+# * class attribute id
+#  * represents a column containing a unique string (60 characters)
+#  * can’t be null
+#  * primary key
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
+            if not kwargs.get('updated_at'):
+                kwargs['updated_at'] = datetime.utcnow()
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+            if not kwargs.get('created_at'):
+                kwargs['created_at'] = datetime.utcnow()
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
@@ -30,8 +41,9 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +53,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        # remove the key _sa_instance_state from the dictionary if it exists:
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """deletes an instance based on its id"""
+        from models import storage
+        storage.delete(self)
