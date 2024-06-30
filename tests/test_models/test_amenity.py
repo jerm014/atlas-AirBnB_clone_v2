ff8ed06011e3534113fedb9ff566ccbdfdc13227
nooboftime <tamara_walling@yahoo.com>
2024-06-30 16:02:16 -0500

    test amenity file finish

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,20 +9,33 @@ from models import storageType
 
 class TestAmenity(unittest.TestCase):
     """Class test for Amenity"""
-
+    
     def setUp(self):
-	"""Set up test environment"""
-	self.amenity = Amenity()
-
-
+        """Set up test environment"""
+        self.amenity = Amenity()
+        
     def test_amenity_is_instance(self):
-	"""Test if Amenity instance is created"""
-	self.assertIsInstance(self.amenity, Amenity)
+        """Test if Amenity instance is created"""
+        self.assertIsInstance(self.amenity, Amenity)
 
 
-    def test_amenity_has_name_attr(self):
-	"""Test if 'name' attribute exists in Amenity instance"""
-self.assertTrue(hasattr(self.amenity, 'name'))
+    def test_amenity_has_name_attr(self):   
+        """Test if 'name' attribute exists in Amenity instance"""
+        self.assertTrue(hasattr(self.amenity, 'name'))
 
 
     def test_amenity_name_initial_value(self):
+        """Test initial value of 'name' attribute"""
+        if storageType == 'db':
+            self.assertEqual(self.amenity.name, None)
+        else:
+            self.assertEqual(self.amenity.name, "")
+
+
+    def test_amenity_name_type(self):
+        """Test type of 'name' attribute"""
+        self.assertIsInstance(self.amenity.name, str if storageType != 'db' else type(None))
+
+
+        if __name__ == '__main__':
+                unittest.main()
