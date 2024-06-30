8b99b0bfcf767d7f3b2678217fc51ecc27f53c02
nooboftime <tamara_walling@yahoo.com>
2024-06-30 14:39:09 -0500

    amenity test self

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-"""Unittests for Amenity Class"""
+"""This module is a set of unit tests for Amenity"""
 import unittest
 import os
-from models.engine file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 from models.amenity import Amenity
 from models import storageType
 
 
 class TestAmenity(unittest.TestCase):
-    """Test the Amenity class"""
+    """Class test for Amenity"""
+
+    def setUp(self):
+	"""Set up test environment"""
+	self.amenity = Amenity()
 
 
+    def test_amenity_is_instance(self):
+	"""Test if Amenity instance is created"""
+	self.assertIsInstance(self.amenity, Amenity)
+
+
+    def test_amenity_has_name_attr(self):
+	"""Test if 'name' attribute exists in Amenity instance"""
+self.assertTrue(hasattr(self.amenity, 'name'))
+
+
+    def test_amenity_name_initial_value(self):
