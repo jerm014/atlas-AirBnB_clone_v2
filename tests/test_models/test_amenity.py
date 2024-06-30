3d43598e4102deb554b889626e4fda79afa0a336
nooboftime <tamara_walling@yahoo.com>
2024-06-30 14:18:32 -0500

    some of amenity test file adding more

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""This module is a set of unit tests for Amenity"""
+"""Unittests for Amenity Class"""
 import unittest
 import os
-from models.engine.file_storage import FileStorage
+from models.engine file_storage import FileStorage
 from models.amenity import Amenity
-
-my_model = Amenity()
+from models import storageType
 
 
 class TestAmenity(unittest.TestCase):
-    """Class test for Amenity"""
+    """Test the Amenity class"""
 
-    def test_amenity(self):
-        """Unit tests for Amenity"""
-        amenity = Amenity()
-        self.assertTrue(hasattr(amenity, 'name'))
-        self.assertEqual(amenity.name, "")
+
