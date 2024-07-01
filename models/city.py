b58ada56b747b1303030948cfbc2c92a13a71886
nooboftime <tamara_walling@yahoo.com>
2024-07-01 11:59:14 -0500

    blank line had whitpace in city py

--- models/city.py
+++ models/city.py
@@ -8,7 +8,7 @@ import os
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    
+
     if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
