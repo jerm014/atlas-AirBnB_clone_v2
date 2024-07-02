6a0c0e5afee5598a421ef18678b635edbd519372
Jeremy Mitts <jermitts@gmail.com>
2024-07-01 20:16:26 -0500

    corrected line 15 to include first name last name and password

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,7 +12,7 @@ class TestHBNBCommand(unittest.TestCase):
         """Test creating a new instance with valid parameters"""
         with patch('sys.stdout', new=StringIO()) as output:
             HBNBCommand().onecmd(
-                'create User name="John Doe" password="hunter2"'
+                'create User first_name="John" last_name="Doe" password="Hunter2"'
                 )
             instance_id = output.getvalue().strip()
             self.assertTrue(len(instance_id) > 0)
