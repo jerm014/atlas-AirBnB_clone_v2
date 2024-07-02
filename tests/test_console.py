13fcd24b5bad9ba4f2928d967031eafae613dd5f
Jeremy Mitts <jermitts@gmail.com>
2024-07-01 20:17:55 -0500

    corected assertions on lines 25-27 to look for first name, last name, and password

--- tests/test_console.py
+++ tests/test_console.py
@@ -22,7 +22,9 @@ class TestHBNBCommand(unittest.TestCase):
             # Verify that the instance has been created with correct attributes
             HBNBCommand().onecmd(f'show User {instance_id}')
             instance_output = output.getvalue().strip()
-            self.assertIn("John Doe", instance_output)
+            self.assertIn("John", instance_output)
+            self.assertIn("Doe", instance_output)
+            self.assertIn("Hunter2", instance_output)
 
     def test_create_with_invalid_class(self):
         """Test creating a new instance with an invalid class"""
