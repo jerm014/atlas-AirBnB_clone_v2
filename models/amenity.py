a6a483dea4c365337e9378f77a4b8e6487d7eb57
nooboftime <tamara_walling@yahoo.com>
2024-07-02 19:30:50 +0000

    added comments for amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -7,9 +7,12 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """ the class for the amentiy """
-    __tablename__ = 'amenities'
+    __tablename__ = 'amenities' # Defines the name of the table in the database
 
+    # Conditional attribute definition based on storage type
     if storage_type == 'db':
+        # Define the 'name' column in the table, with a max length of 128 characters, and cannot be null
         name = Column(String(128), nullable=False)
     else:
+        # If storage is not database, set 'name' as an empty string
         name = ""
