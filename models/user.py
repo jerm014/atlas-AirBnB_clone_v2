4a1f806008e4c6c4b56705207bbf1ff311b9598d
nooboftime <tamara_walling@yahoo.com>
2024-07-02 20:27:47 +0000

    comments for user.py

--- models/user.py
+++ models/user.py
@@ -7,16 +7,23 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = 'users' # Defines the table name in the database
 
     if storage_type == 'db':
+        # Define 'email' column, max length 128 characters, cannot be null
         email = Column(String(128), nullable=False, default="")
+        # Define 'password' column, max length 128 characters, cannot be null
         password = Column(String(128), nullable=False, default="")
+        # Define 'first_name' column, max length 128 characters, can be null
         first_name = Column(String(128), nullable=True, default="")
+        # Define 'last_name' column, max length 128 characters, can be nul
         last_name = Column(String(128), nullable=True, default="")
+        # User has many places, each place has a back reference to user
         places = relationship("Place", backref="user")
+        # User has many reviews, each review has a back reference to user
         reviews = relationship("Review", backref="user")
     else:
+        # If storage is not a database, set attributes to empty strings
         email = ''
         password = ''
         first_name = ''
@@ -24,4 +31,4 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """Initializes user"""
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs) # Calls the super class init method
