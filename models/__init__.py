7874dc3450d1e822acc335434bad1aedda331494
nooboftime <tamara_walling@yahoo.com>
2024-07-01 22:37:34 -0500

    changed init file back

--- models/__init__.py
+++ models/__init__.py
@@ -6,19 +6,13 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.user import User
-import os
 
-
-# Fetch the storage type from the environment variable
-storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-elif storage_type == 'fs':
+else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-else:
-    raise ValueError("Invalid storage type")
 
 storage.reload()
