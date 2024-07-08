1081b05abbcd19c791d35edfa7bf333bfc088888
Jeremy Mitts <jermitts@gmail.com>
2024-07-08 17:22:56 -0500

    project 2130, task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" task 7 """
+""" task 9 """
 from flask import Flask, render_template
 
 
@@ -56,9 +56,17 @@ def states_list():
     return render_template('7-states_list.html', states=sorted_states)
 
 
-def get_name(item):
-    key, value = item
-    return value.name
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    from models import storage
+    from models.state import State
+    from models.city import City
+
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    cities = storage.all(City).values()
+    sorted_cities = sorted(cities, key=lambda city: city.name)
+    return render_template('8-cities_by_states.html', states=sorted_states, cities = sorted_cities)
 
 
 @app.teardown_appcontext
